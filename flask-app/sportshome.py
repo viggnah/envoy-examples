d76cf79a4d6989e61fec593a28c157b49776adc0
viggnah <viggnahasd@gmail.com>
2022-12-10 11:21:21 +0530

    Refactored for readability

--- flask-app/sportshome.py
+++ flask-app/sportshome.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_world():
+def live_sports_scores():
     games = []
     if "cricketScores" in request.form:
         games = get_cricket_scores()
@@ -20,36 +20,36 @@ def get_cricket_scores():
     ongoing_scores = ["58/2, 6.2 Overs", "95/4, 14.5 Overs", "85/4, 17.5 Overs", "70/1, 8.4 Overs"]
 
     games = []
-    picked_teams = []
-    for  i in range(random.randint(1, 2)):
-        while True:
-            t1, t2 = random.sample(list(range(len(teams))), 2)
-            if t1 not in picked_teams and t2 not in picked_teams:
-                picked_teams.extend([t1, t2])
-                break
+    picked_indexes = []
+    for _ in range(random.randint(1, 2)):
+        t1_index, t2_index = pick_random_nos_without_replacement(picked_indexes, teams)
+        picked_indexes.extend([t1_index, t2_index])
 
-        team1 = Team(teams[t1], completed_scores[t1])
-        team2 = Team(teams[t2], ongoing_scores[t2])
+        team1 = Team(teams[t1_index], completed_scores[t1_index])
+        team2 = Team(teams[t2_index], ongoing_scores[t2_index])
         games.append(Game(team1, team2))
-    
+
     return games
 
+def pick_random_nos_without_replacement(picked_indexes, teams):
+    while True:
+        t1_index, t2_index = random.sample(list(range(len(teams))), 2)
+        if t1_index not in picked_indexes and t2_index not in picked_indexes:
+            break
+    return t1_index, t2_index
 
 def get_football_scores():
     teams = ["Argentina", "Australia", "England", "Brazil"]
     scores = ["0", "1", "2", "3", "4"] 
 
     games = []
-    picked_teams = []
-    for  i in range(random.randint(1, 2)):
-        while True:
-            t1, t2 = random.sample(list(range(len(teams))), 2)
-            if t1 not in picked_teams and t2 not in picked_teams:
-                picked_teams.extend([t1, t2])
-                break
+    picked_indexes = []
+    for _ in range(random.randint(1, 2)):
+        t1_index, t2_index = pick_random_nos_without_replacement(picked_indexes, teams)
+        picked_indexes.extend([t1_index, t2_index])
 
-        team1 = Team(teams[t1], scores[t1])
-        team2 = Team(teams[t2], scores[t2])
+        team1 = Team(teams[t1_index], scores[t1_index])
+        team2 = Team(teams[t2_index], scores[t2_index])
         games.append(Game(team1, team2))
     
     return games
